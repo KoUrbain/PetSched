54343e8cb7ed04db231c1331dc4fc9cd0b53bd8a
KoUrbain <okodj030@uottawa.ca>
2025-09-30 20:00:02 -0400

    debug2

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,14 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
+    // SDK 50: expo-router/babel is deprecated; babel-preset-expo covers it
     presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel', 'react-native-reanimated/plugin']
+    // Keep Reanimated plugin last
+    plugins: [
+      'react-native-reanimated/plugin',
+      // optional: module-resolver for TS path aliases
+      // ['module-resolver', { alias: { '@lib': './lib', '@components': './components', '@store': './store', '@app': './app' } }],
+    ],
   };
 };
